2a6dfb0936dbd325c9d9af79fb8aa592697a173d
inotaku <i_no_u_u@yahoo.co.jp>
2014-10-15 17:06:50 +0900

    Session追加

--- ShoppingSiteGradle/src/main/java/jp/com/inotaku/controller/ItemController.java
+++ ShoppingSiteGradle/src/main/java/jp/com/inotaku/controller/ItemController.java
@@ -1,5 +1,8 @@
 package jp.com.inotaku.controller;
 
+import javax.servlet.http.HttpSession;
+
+import jp.com.inotaku.domain.User;
 import jp.com.inotaku.service.ItemService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +18,22 @@ public class ItemController {
 	private ItemService itemService;
 
 	@RequestMapping(value = "/item",method = RequestMethod.GET)
-	public String index(Model model){
+	public String index(Model model,HttpSession session){
 		model.addAttribute("itemList",itemService.getItemList());
+		User loginUser = (User)session.getAttribute("user");
+		if(loginUser != null){
+			model.addAttribute("loginUser",loginUser);
+		}
 		return "index";
 	}
 	
 	@RequestMapping(value = "/detail",method = RequestMethod.GET)
-	public String detail(long itemId, Model model){
+	public String detail(long itemId, Model model,HttpSession session){
 		model.addAttribute("item",itemService.getItemByItemId(itemId));
+		User loginUser = (User)session.getAttribute("user");
+		if(loginUser != null){
+			model.addAttribute("loginUser",loginUser);
+		}
 		return "detail";
 	}
 }
