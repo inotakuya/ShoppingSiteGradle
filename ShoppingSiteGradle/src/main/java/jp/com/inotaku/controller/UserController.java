6bd8c0a5609113b3f1e83ccc34bb34eaf70a2d34
inotaku <i_no_u_u@yahoo.co.jp>
2014-10-15 17:03:17 +0900

    Session追加

--- ShoppingSiteGradle/src/main/java/jp/com/inotaku/controller/UserController.java
+++ ShoppingSiteGradle/src/main/java/jp/com/inotaku/controller/UserController.java
@@ -1,5 +1,8 @@
 package jp.com.inotaku.controller;
 
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
 
 import jp.com.inotaku.domain.User;
 import jp.com.inotaku.domain.User.userEntry;
@@ -13,6 +16,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 public class UserController {
@@ -21,22 +26,26 @@ public class UserController {
 	private UserService userService;
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String toLoginView(Model model) {
-		model.addAttribute("user", new User());
+	public String toLoginView(Model model,HttpSession session) {
+		model.addAttribute( new User());
+		model.addAttribute("loginUser","login");
+		session.invalidate();
 		return "login";
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String onSubmit(@Validated(LoginForm.class) User user,
-			BindingResult bindingResult, Model model) {
+			BindingResult bindingResult, Model model, HttpSession session) {
 
 		if (bindingResult.hasErrors()) {
+			model.addAttribute("loginUser","login");
 			return "login";
 		}
 
 		try {
 			User loginUser = userService.getUserByUserNameAndPassword(
 					user.getUserName(), user.getPassword());
+			session.setAttribute("user", loginUser);
 			model.addAttribute("loginUser", loginUser);
 		} catch (Exception e) {
 			return "login";
@@ -45,18 +54,27 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "userEntry", method = RequestMethod.GET)
-	public String toUserEntryView(Model model) {
-		model.addAttribute("user", new User());
+	public String toUserEntryView(Model model,HttpSession session) {
+		model.addAttribute(new User());
+		session.invalidate();
 		return "userEntry";
 	}
 
 	@RequestMapping(value = "userEntry", method = RequestMethod.POST)
-	public String addUser(@Validated(userEntry.class) User user, BindingResult bindingResult, Model model) {
-		if(bindingResult.hasErrors()){
+	public String addUser(@Validated(userEntry.class) User user,
+			BindingResult bindingResult, Model model, HttpSession session) {
+		if (bindingResult.hasErrors()) {
 			return "userEntry";
 		}
 		userService.addUser(user);
-		model.addAttribute("user",user);
+		session.setAttribute("user", user);
+		model.addAttribute("loginUser",user);
 		return "userEntrySuccess";
+	}
+	
+	@RequestMapping(value = "logout", method = RequestMethod.GET)
+	public String logout(Model model,HttpSession session){
+		session.invalidate();
+		return "redirect:login";
 	}
 }
